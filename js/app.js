909752cda4115560e885ceb47d1d3401605fb78c
mhairimccrindle@gmail.com <mhairimccrindle@gmail.com>
2019-08-25 16:13:38 +0100

    now with spelling mistakes fixed

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-const form = document.querySelector('#new-entry-form');
-const currentMembers = document.querySelector('#members');
+const form = document.querySelector('#new-member-form');
+const currentMembers = document.querySelector('#currentMembers');
 
 form.addEventListener('submit', (event) => {
 
@@ -11,14 +11,28 @@ form.addEventListener('submit', (event) => {
   const position = event.target.position.value;
   const memberType = event.target.memberType.value;
 
-  newEntry.textContent = `Name: ${name}, Bike: ${bike}, Riding Position: ${position}, Member Level: ${memberType}`;
-  newEntry.classList.add('entry');
+  const newEntry = document.createElement('p')
+    newEntry.textContent = `Name: ${name}, Bike: ${bike}, Riding Position: ${position}, Member Level: ${memberType}`;
+    newEntry.classList.add('entry');
 
   currentMembers.appendChild(newEntry);
 
   form.reset();
 
 });
+
+const deleteButton = document.createElement('button');
+ deleteButton.textContent = `Delete`;
+ deleteButton.addEventListener('click', () => {
+   const remove = document.querySelectorAll('.entry');
+   for ( entry of remove) {
+     console.log(entry);
+     entry.remove();
+   }
+ })
+
+currentMembers.appendChild(deleteButton);
+
 
 
 
